09843df2be09235a9f143cd617b19bf5c82aeb68
Skwangles <skwanglesavenue@gmail.com>
2021-08-03 22:15:22 +1200

    Checking bottom right corner pixel for specific blue, if found, clicks. Need to find a way to handle Quarantine, because I don't have anything to quarantine. Added comments. Next is Malwarebytes. Need to check with different screen sizes & deploying on different systems.

--- main.py
+++ main.py
@@ -5,37 +5,55 @@ import keyboard
 import win32api
 import subprocess
 from win32api import GetSystemMetrics
-iagree = [360, 373] #Location of the I Agree button constant
-scan = [416, 218] #Location of the Scan Now button Constant
+
+i_agree = [360, 373] # Location of the I Agree button constant
+scan = [416, 218] # Location of the Scan Now button Constant
+corner_button = [602, 425]
 metrics = [GetSystemMetrics(0), GetSystemMetrics(1)]
-def mwb():
+bottom_right_blue = 174
+
+#
+#
+# -------------------------AdwCleaner-----------------------------
+#
+#
+
+
+def adw_cleaner_run():
     time.sleep(2)
     print("Started")
-    subprocess.call(["C:\\Users\\tree_\\Downloads\\adwcleaner_8.3.0.exe"])  #Open adwcleaner
     pyautogui.confirm("Click OK if 1. AdwCleaner is open, 2. All other apps are closed.")
-    coord = pyautogui.locateCenterOnScreen("buggy.png", region=(0, 0, GetSystemMetrics(0)/2, GetSystemMetrics(1)/2))  #Find top left bug
+    # Find top left bug icon
+    coord = pyautogui.locateCenterOnScreen("buggy.png", region=(0, 0, GetSystemMetrics(0)/2, GetSystemMetrics(1)/2))
     tap_i_agree(coord)
     tap_scan(coord)
-    time.sleep(20)  #Wait for clean
-    sbr_coords = pyautogui.locateCenterOnScreen("skipb.png", region=(
-        coord[0], coord[1], metrics[0], metrics[1]
-    ))
-    print(sbr_coords)
+    time.sleep(5)  # Wait for clean
+    pixel_at_corner = pyautogui.pixel(coord[0] + corner_button[0], coord[1] + corner_button[1])
+    while pixel_at_corner[2] != bottom_right_blue:  # Waits until done
+        time.sleep(5)
+        pixel_at_corner = pyautogui.pixel(coord[0] + corner_button[0], coord[1] + corner_button[1])
+    # Move to Skip/Quarantine & Click
+    pyautogui.moveTo(coord[0] + corner_button[0], coord[1] + corner_button[1], duration=0)
+    pyautogui.click()
 
 
 def tap_i_agree(coord):
-    pyautogui.moveTo(coord[0] + iagree[0], coord[1] + iagree[1], duration=0)
+    pyautogui.moveTo(coord[0] + i_agree[0], coord[1] + i_agree[1], duration=0)
     pyautogui.click()
 
 
 def tap_scan(coord):
     pyautogui.moveTo(coord[0] + scan[0], coord[1] + scan[1], duration=0)
     pyautogui.click()
-
+#
+# ------------------------ END of AdwCleaner ------------------------------------
+#
 
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
     pyautogui.FAILSAFE = True
-    mwb()
+    adw_cleaner_run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
