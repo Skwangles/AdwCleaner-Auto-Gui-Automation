cf676f7fd2cb15542b455235d4c5c5ed4bcf187f
Skwangles <skwanglesavenue@gmail.com>
2021-08-04 16:03:42 +1200

    fixed .pixel error. Implemented findbug method for the case that the window is moved during install - the app updates its coord variable before each major block of tasks

--- main.py
+++ main.py
@@ -24,35 +24,43 @@ def adw_cleaner_run():
     print("Started")
     pyautogui.confirm("Click OK if 1. AdwCleaner is open, 2. All other apps are closed.")
     # Find top left bug icon
-    coord = pyautogui.locateCenterOnScreen('buggy.png', region=(0, 0, GetSystemMetrics(0) / 2, GetSystemMetrics(1) / 2))
-    #
+    tap_i_agree(find_bug())
+    tap_scan(find_bug())
+    time.sleep(5)  # Wait for clean
+    # pyautogui.moveTo(find_bug()[0] + corner_button[0], find_bug()[1] + corner_button[1], duration=3)
+    # time.sleep(3)
+    # pyautogui.moveTo(30, 30, duration=0)
+    coord = find_bug()
+    find_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
+    pixel_at_corner = find_blue_screenshot.getpixel(((coord[0] + corner_button[0]), (coord[1] + corner_button[1])))
+    while pixel_at_corner[2] != bottom_right_blue:  # Waits until done
+        time.sleep(3)
+        coord = find_bug()
+        find_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
+        pixel_at_corner = find_blue_screenshot.getpixel(((corner_button[0]),
+                                                         (corner_button[1])))
+        print("Button not blue")
+    # Move to Skip/Quarantine & Click
+    print("Button IS blue - clicking")
+    pyautogui.click(find_bug()[0] + corner_button[0], find_bug()[1] + corner_button[1])
+
+
+def find_bug():
+    coord = pyautogui.locateCenterOnScreen('buggy.png')
     while coord is None:
-        time.sleep(5)
+        time.sleep(1)
         coord = pyautogui.locateCenterOnScreen('buggy.png',
                                                region=(0, 0, GetSystemMetrics(0) / 2, GetSystemMetrics(1) / 2))
-    tap_i_agree(coord)
-    tap_scan(coord)
-    time.sleep(5)  # Wait for clean
-    pyautogui.moveTo(coord[0] + corner_button[0], coord[1] + corner_button[1], duration=3)
-    time.sleep(3)
-    pyautogui.moveTo(30, 30, duration=0)
-    pixel_at_corner = pyautogui.pixel(coord[0] + corner_button[0], coord[1] + corner_button[1])
-    while pixel_at_corner[2] != bottom_right_blue:  # Waits until done
-        time.sleep(5)
-        pixel_at_corner = pyautogui.pixel(coord[0] + corner_button[0], coord[1] + corner_button[1])[2]
-    # Move to Skip/Quarantine & Click
-    pyautogui.moveTo(coord[0] + corner_button[0], coord[1] + corner_button[1], duration=0)
-    pyautogui.click()
+    print("Icon found")
+    return coord
 
 
 def tap_i_agree(coord):
-    pyautogui.moveTo(coord[0] + i_agree[0], coord[1] + i_agree[1], duration=0)
-    pyautogui.click()
+    pyautogui.click(coord[0] + i_agree[0], coord[1] + i_agree[1])
 
 
 def tap_scan(coord):
-    pyautogui.moveTo(coord[0] + scan[0], coord[1] + scan[1], duration=0)
-    pyautogui.click()
+    pyautogui.click(coord[0] + scan[0], coord[1] + scan[1])
 #
 # ------------------------ END of AdwCleaner ------------------------------------
 #
