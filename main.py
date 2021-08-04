f1723d4d44ac151f7f1d4f2f75a3e8f4ddc0af8b
Skwangles <skwanglesavenue@gmail.com>
2021-08-04 16:37:20 +1200

    implemented the adwClean into a single class, so to keep the cleaning operations seperate from one another. Need to attempt to deploy on a system without python installed to test if it can be applied to anything.

--- main.py
+++ main.py
@@ -19,46 +19,57 @@ correct_blue_value = 174
 #
 
 
-def adw_cleaner_run():
-    time.sleep(2)
-    print("Started")
-    pyautogui.confirm("Click OK if 1. AdwCleaner is open, 2. All other apps are closed.")
-    # Find top left bug icon
-    tap_i_agree(locate_image("bug"))
-    tap_scan(locate_image("bug"))
-    time.sleep(5)  # Wait for clean
-    coord = locate_image("bug")
-    check_for_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
-    pixel_at_corner = check_for_blue_screenshot.getpixel(((coord[0] + corner_button[0]), (coord[1] + corner_button[1])))
-    while pixel_at_corner[2] is not correct_blue_value:  # Waits until done
-        time.sleep(3)  # Wait before trying again
-        coord = locate_image("bug")
-        # Screenshot of smaller area to save resources
-        check_for_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
-        pixel_at_corner = check_for_blue_screenshot.getpixel(
-            ((corner_button[0]), (corner_button[1]))
-            # Coordinates are in a smaller size because of screenshot starting at bug icon
-        )
-        print("Button not blue")
-    # Move to Skip/Quarantine & Click
-    print("Button IS blue - clicking")
-    if locate_image("Ok") is not None:  # If it detects an "Ok" button will click and prompt user
-        ok_coords = locate_image("Ok")
-        pyautogui.click(ok_coords[0], ok_coords[1])
-        pyautogui.alert("Please select all items to quarantine - then press OK")
+class adwCleaner:
 
-    pyautogui.click(locate_image("bug")[0] + corner_button[0], locate_image("bug")[1] + corner_button[1])
+    @classmethod
+    def adw_cleaner_run(cls):
+        time.sleep(2)
+        print("Started")
+        pyautogui.confirm("Click OK if 1. AdwCleaner is open, 2. All other apps are closed.")
+        # Find top left bug icon
+        cls.tap_i_agree(locate_image("bug"))
+        cls.tap_scan(locate_image("bug"))
+        time.sleep(5)  # Wait for clean
+        coord = locate_image("bug")
+        check_for_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
+        pixel_at_corner = check_for_blue_screenshot.getpixel(((coord[0] + corner_button[0]), (coord[1] + corner_button[1])))
+        while pixel_at_corner[2] is not correct_blue_value:  # Waits until done
+            time.sleep(3)  # Wait before trying again
+            coord = locate_image("bug")
+            # Screenshot of smaller area to save resources
+            check_for_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
+            pixel_at_corner = check_for_blue_screenshot.getpixel(
+                ((corner_button[0]), (corner_button[1]))
+                # Coordinates are in a smaller size because of screenshot starting at bug icon
+            )
+            print("Button not blue")
+        # Move to Skip/Quarantine & Click
+        print("Button IS blue - clicking")
+        if locate_image("Ok") is not None:  # If it detects an "Ok" button will click and prompt user
+            ok_button_coordinates = locate_image("Ok")
+            pyautogui.click(ok_button_coordinates[0], ok_button_coordinates[1])
+            pyautogui.alert("Please select all items to quarantine - then press OK")
+
+        pyautogui.click(locate_image("bug")[0] + corner_button[0], locate_image("bug")[1] + corner_button[1])
+
+    @classmethod
+    def tap_i_agree(cls, coord):
+        pyautogui.click(coord[0] + i_agree[0], coord[1] + i_agree[1])
+
+    @classmethod
+    def tap_scan(cls, coord):
+        pyautogui.click(coord[0] + scan[0], coord[1] + scan[1])
 
 
 def locate_image(image):
-    if image is "bug":
+    if image == "bug":
         coord = pyautogui.locateCenterOnScreen('buggy.png')
         while coord is None:
             time.sleep(1)
             coord = pyautogui.locateCenterOnScreen(
                 'buggy.png', region=(0, 0, GetSystemMetrics(0) / 2, GetSystemMetrics(1) / 2)
             )
-    elif image is "Ok":
+    elif image == "Ok":
         coord = pyautogui.locateCenterOnScreen('OK.png')
         return coord
     else:
@@ -66,23 +77,13 @@ def locate_image(image):
         raise Exception("locate_image was called with an image not in our list!")
     print("Image coordinates located")
     return coord
-
-
-def tap_i_agree(coord):
-    pyautogui.click(coord[0] + i_agree[0], coord[1] + i_agree[1])
-
-
-def tap_scan(coord):
-    pyautogui.click(coord[0] + scan[0], coord[1] + scan[1])
-#
 # ------------------------ END of AdwCleaner ------------------------------------
 #
-
 # Press the green button in the gutter to run the script.
 
 
 if __name__ == '__main__':
     pyautogui.FAILSAFE = True
-    adw_cleaner_run()
+    adwCleaner().adw_cleaner_run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
