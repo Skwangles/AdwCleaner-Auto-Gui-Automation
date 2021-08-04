2da4a685f6a23d8b9c6c9423c81fd06b5c1e327b
Skwangles <skwanglesavenue@gmail.com>
2021-08-04 16:22:24 +1200

    made find_bug method handle all image location with "pythonic" switch case. Implemented OK wait and click

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ i_agree = [360, 373]  # Location of the I Agree button constant
 scan = [416, 218]  # Location of the Scan Now button Constant
 corner_button = [602, 425]
 metrics = [GetSystemMetrics(0), GetSystemMetrics(1)]
-bottom_right_blue = 174
+correct_blue_value = 174
 
 #
 #
@@ -24,34 +24,47 @@ def adw_cleaner_run():
     print("Started")
     pyautogui.confirm("Click OK if 1. AdwCleaner is open, 2. All other apps are closed.")
     # Find top left bug icon
-    tap_i_agree(find_bug())
-    tap_scan(find_bug())
+    tap_i_agree(locate_image("bug"))
+    tap_scan(locate_image("bug"))
     time.sleep(5)  # Wait for clean
-    # pyautogui.moveTo(find_bug()[0] + corner_button[0], find_bug()[1] + corner_button[1], duration=3)
-    # time.sleep(3)
-    # pyautogui.moveTo(30, 30, duration=0)
-    coord = find_bug()
-    find_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
-    pixel_at_corner = find_blue_screenshot.getpixel(((coord[0] + corner_button[0]), (coord[1] + corner_button[1])))
-    while pixel_at_corner[2] != bottom_right_blue:  # Waits until done
-        time.sleep(3)
-        coord = find_bug()
-        find_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
-        pixel_at_corner = find_blue_screenshot.getpixel(((corner_button[0]),
-                                                         (corner_button[1])))
+    coord = locate_image("bug")
+    check_for_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
+    pixel_at_corner = check_for_blue_screenshot.getpixel(((coord[0] + corner_button[0]), (coord[1] + corner_button[1])))
+    while pixel_at_corner[2] is not correct_blue_value:  # Waits until done
+        time.sleep(3)  # Wait before trying again
+        coord = locate_image("bug")
+        # Screenshot of smaller area to save resources
+        check_for_blue_screenshot = pyautogui.screenshot(region=(coord[0], coord[1], metrics[0], metrics[1]))
+        pixel_at_corner = check_for_blue_screenshot.getpixel(
+            ((corner_button[0]), (corner_button[1]))
+            # Coordinates are in a smaller size because of screenshot starting at bug icon
+        )
         print("Button not blue")
     # Move to Skip/Quarantine & Click
     print("Button IS blue - clicking")
-    pyautogui.click(find_bug()[0] + corner_button[0], find_bug()[1] + corner_button[1])
+    if locate_image("Ok") is not None:  # If it detects an "Ok" button will click and prompt user
+        ok_coords = locate_image("Ok")
+        pyautogui.click(ok_coords[0], ok_coords[1])
+        pyautogui.alert("Please select all items to quarantine - then press OK")
+
+    pyautogui.click(locate_image("bug")[0] + corner_button[0], locate_image("bug")[1] + corner_button[1])
 
 
-def find_bug():
-    coord = pyautogui.locateCenterOnScreen('buggy.png')
-    while coord is None:
-        time.sleep(1)
-        coord = pyautogui.locateCenterOnScreen('buggy.png',
-                                               region=(0, 0, GetSystemMetrics(0) / 2, GetSystemMetrics(1) / 2))
-    print("Icon found")
+def locate_image(image):
+    if image is "bug":
+        coord = pyautogui.locateCenterOnScreen('buggy.png')
+        while coord is None:
+            time.sleep(1)
+            coord = pyautogui.locateCenterOnScreen(
+                'buggy.png', region=(0, 0, GetSystemMetrics(0) / 2, GetSystemMetrics(1) / 2)
+            )
+    elif image is "Ok":
+        coord = pyautogui.locateCenterOnScreen('OK.png')
+        return coord
+    else:
+        alert("That image is not in our list!")
+        raise Exception("locate_image was called with an image not in our list!")
+    print("Image coordinates located")
     return coord
 
 
